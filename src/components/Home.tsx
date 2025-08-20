5c5484f833a03ad8a0a0d0862afe75e2b6aa5579
religionheaven <ta0aslam@gmail.com>
2025-08-20 03:45:47 -0500

    Fix syntax error in Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -455,46 +455,7 @@ function MobileReligionCards({
       </div>
     </div>
   );
-    {/* Mobile Confessions Button - positioned much lower */}
-    <div className="relative z-20 flex justify-center mt-8 md:hidden">
-function HomeContent({
-  onReligionClick,
-  onShowRequests,
-  onShowAdmin,
-  onHideRequests,
-  onSignOut,
-  isTransitioning,
-  clickCounts,
-  loadingCounts,
-  showProfileModal,
-  setShowProfileModal,
-  userProfile,
-  onProfileUpdate,
-  showLiveChat,
-  setShowLiveChat,
-  showRequestedReligions,
-  setShowRequestedReligions,
-  confessions,
-  confessionText,
-  setConfessionText,
-  isSubmittingConfession,
-  handleSubmitConfession,
-  confessionSortBy,
-  setConfessionSortBy,
-  loadingConfessions,
-  handleVoteOnConfession,
-  formatTimeAgo,
-  showConfessions,
-  setShowConfessions,
-  showWarning,
-  warningMessage,
-  isWarningFadingOut,
-}: HomeContentProps) {
-  const getClickCount = (religion: string) => {
-    if (loadingCounts) return '...';
-    const found = clickCounts.find((item) => item.religion === religion);
-    return found?.click_count || 0;
-  };
+  }
 
   return (
     <div className="min-h-screen relative overflow-hidden bg-white">
@@ -571,7 +532,7 @@ function HomeContent({
         </p>
       </div>
 
-      {/* Mobile Requested Religion and Confessions Buttons - positioned below "powered by heaven" */}
+      {/* Mobile Requested Religion Button - positioned below "powered by heaven" */}
       <div className="relative z-20 flex justify-center mt-4 md:hidden">
         <div className="flex flex-col items-center gap-2">
           <button
@@ -584,6 +545,47 @@ function HomeContent({
           </button>
         </div>
       </div>
+
+      {/* Mobile Confessions Button - positioned much lower */}
+      <div className="relative z-20 flex justify-center mt-8 md:hidden">
+function HomeContent({
+  onReligionClick,
+  onShowRequests,
+  onShowAdmin,
+  onHideRequests,
+  onSignOut,
+  isTransitioning,
+  clickCounts,
+  loadingCounts,
+  showProfileModal,
+  setShowProfileModal,
+  userProfile,
+  onProfileUpdate,
+  showLiveChat,
+  setShowLiveChat,
+  showRequestedReligions,
+  setShowRequestedReligions,
+  confessions,
+  confessionText,
+  setConfessionText,
+  isSubmittingConfession,
+  handleSubmitConfession,
+  confessionSortBy,
+  setConfessionSortBy,
+  loadingConfessions,
+  handleVoteOnConfession,
+  formatTimeAgo,
+  showConfessions,
+  setShowConfessions,
+  showWarning,
+  warningMessage,
+  isWarningFadingOut,
+}: HomeContentProps) {
+  const getClickCount = (religion: string) => {
+    if (loadingCounts) return '...';
+    const found = clickCounts.find((item) => item.religion === religion);
+    return found?.click_count || 0;
+  };
 
       {/* Mobile Confessions Button - positioned below "powered by heaven" */}
       <div className="relative z-20 flex justify-center mt-2 md:hidden">
