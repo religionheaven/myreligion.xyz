8da18103ba1845ecbae4200662b591108455cb28
religionheaven <ta0aslam@gmail.com>
2025-08-22 01:54:52 -0500

    Update Discovery tab with SDK integration message

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -635,9 +635,7 @@ function HomeContent({
             {selectedTool === 'avatar' && <AvatarTool />}
             {selectedTool === 'discovery' && (
               <div className="text-center">
-                <p className="text-white/60" style={{ fontFamily: 'Poiret One, sans-serif' }}>
-                  SDK Integration coming soon to help you find the best upcoming runners on heaven.xyz
-                </p>
+                <p className="text-white/60">Discovery tool coming soon...</p>
               </div>
             )}
           </div>
