e0ad009ee87c4359cda36fe18907e81cca64f930
religionheaven <ta0aslam@gmail.com>
2025-08-22 03:26:54 -0500

    Updated ToolsSection.tsx

--- src/components/home/ToolsSection.tsx
+++ src/components/home/ToolsSection.tsx
@@ -34,7 +34,7 @@ export function ToolsSection({
 
       {/* Tool Navigation Buttons */}
       <div
-        className={`absolute top-12 left-8 z-25 hidden md:flex transition-all duration-500 ease-in-out ${
+        className={`absolute top-20 left-8 z-25 hidden md:flex transition-all duration-500 ease-in-out ${
           showTools ? 'opacity-100 scale-100' : 'opacity-0 scale-95 pointer-events-none'
         }`}
       >
