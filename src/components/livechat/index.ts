043bda3a54167ebb058b664bf54fb091e3064d45
religionheaven <ta0aslam@gmail.com>
2025-08-22 04:02:08 -0500

    Create barrel export for livechat components

--- src/components/livechat/index.ts
+++ src/components/livechat/index.ts
@@ -3,10 +3,7 @@ export { ChatMessage } from "./ChatMessage";
 export { ChatInput } from "./ChatInput";
 export { WarningPopup } from "./WarningPopup";
 export { checkRateLimit, detectSpam, detectProhibitedContent } from "./SpamDetection";
+export { formatMessageContent } from "./MessageFormatter";
 export * from "./types";
 export * from "./constants";
 export * from "./utils";
-export { default } from "./LiveChat";
-
-
-export default from
