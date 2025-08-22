c7e56001256f612c1858031adb6527958beb9183
religionheaven <ta0aslam@gmail.com>
2025-08-22 03:53:51 -0500

    Fix MessageFormatter export in livechat index

--- src/components/livechat/index.ts
+++ src/components/livechat/index.ts
@@ -2,7 +2,6 @@ export { default as LiveChat } from './LiveChat';
 export { ChatMessage } from './ChatMessage';
 export { ChatInput } from './ChatInput';
 export { WarningPopup } from './WarningPopup';
-export { MessageFormatter } from './MessageFormatter';
 export { SpamDetection } from './SpamDetection';
 export * from './types';
 export * from './constants';
