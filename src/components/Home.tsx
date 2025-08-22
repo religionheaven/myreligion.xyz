8610a90a4b6505dd140fcda0ff6e4d1579a4635b
religionheaven <ta0aslam@gmail.com>
2025-08-22 02:49:02 -0500

    Fix orphaned style object causing React error

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -317,12 +317,6 @@ export function Home({
           <div className="w-full h-full flex items-center justify-center px-8">
             <MobileRequestedReligionCards
               onReligionClick={handleReligionClick}
-              getClickCount={getClickCount}
-            />
-          </div>
-        </div>
-      )}
-
       {/* All Buttons */}
       <HomeButtons
         showRequestedReligions={showRequestedReligions}
